77aac21ab8e732a659cec4867d94539f77b0f6b5
Santiago Christensen <santiago@sitch.com>
2018-04-10 13:06:14 +0200

    Remove filters and queries

--- got/seasons/schema.py
+++ got/seasons/schema.py
@@ -1,5 +1,4 @@
 from graphene import AbstractType, Node, Int, Float
-from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 from got.seasons.models import Season
 
@@ -8,12 +7,6 @@ class SeasonObject(DjangoObjectType):
     class Meta:
         model = Season
         interfaces = (Node,)
-        filter_fields = ['number']
 
     number = Int(description="The number of the season")
     rating = Float(description="Rating of the season in the US (millions)")
-
-
-class Query(AbstractType):
-    season = Node.Field(SeasonObject)
-    seasons = DjangoFilterConnectionField(SeasonObject)
