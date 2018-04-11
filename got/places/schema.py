5a9923186dbf0ed1c7d2e09af15254873897006f
Santiago Christensen <santiago@sitch.com>
2018-04-11 11:26:35 +0200

    Add places query, change fixtures

--- got/places/schema.py
+++ got/places/schema.py
@@ -1,5 +1,6 @@
 from graphene import AbstractType, Node, String
 from graphene_django.types import DjangoObjectType
+from graphene_django import DjangoConnectionField
 
 from got.places.models import Place
 
@@ -11,3 +12,6 @@ class PlaceObject(DjangoObjectType):
 
     name = String(description="The name of the place")
     region = String(description="The region of the place")
+
+class Query(AbstractType):
+    places = DjangoConnectionField(PlaceObject)
